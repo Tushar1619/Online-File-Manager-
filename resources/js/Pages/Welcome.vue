e411cbca96de17416252f53041b6ac45202ba12d
Naidu2404 <venkateswaranaidumoyya@gmail.com>
2024-03-08 14:37:16 +0530

    get started button fix

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -3,90 +3,93 @@ import { Head, Link } from "@inertiajs/vue3";
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import PirmaryButtom from "@/Components/PrimaryButton.vue";
 defineProps({
-  canLogin: {
-    type: Boolean,
-  },
-  canRegister: {
-    type: Boolean,
-  },
-  laravelVersion: {
-    type: String,
-    required: true,
-  },
-  phpVersion: {
-    type: String,
-    required: true,
-  },
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
 });
 </script>
 
 <template>
-  <div class="min-h-screen bg-dots-darker bg-center bg-gray-100">
-    <div class="flex justify-between p-6">
-      <div>
-        <ApplicationLogo />
-        RCRM
-      </div>
+    <div class="min-h-screen bg-dots-darker bg-center bg-gray-100">
+        <div class="flex justify-between p-6">
+            <div>
+                <ApplicationLogo />
+                RCRM
+            </div>
 
-      <div v-if="canLogin" class="">
-        <Link
-          v-if="$page.props.auth.user"
-          :href="route('dashboard')"
-          class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-          >Dashboard</Link
-        >
+            <div v-if="canLogin" class="">
+                <Link
+                    v-if="$page.props.auth.user"
+                    :href="route('dashboard')"
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    >Dashboard</Link
+                >
 
-        <template v-else>
-          <Link
-            :href="route('login')"
-            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-            >Log in</Link
-          >
+                <template v-else>
+                    <Link
+                        :href="route('login')"
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >Log in</Link
+                    >
 
-          <Link
-            v-if="canRegister"
-            :href="route('register')"
-            class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-            >Register</Link
-          >
-        </template>
-      </div>
-    </div>
-    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-      <div class="text-center">
-        <h1 class="text-4xl font-extrabold text-gray-900 sm:text-5xl">
-          Welcome to RCRM Storage
-        </h1>
-        <p class="mt-4 text-lg text-gray-600">
-          Store, share, and collaborate on files and folders from your mobile
-          device, tablet, or computer
-        </p>
-        <div class="mt-6">
-          <p
-            class="text-base font-medium text-indigo-600 hover:text-indigo-500"
-          >
-            We are delighted to have you here!
-          </p>
+                    <Link
+                        v-if="canRegister"
+                        :href="route('register')"
+                        class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-black focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >Register</Link
+                    >
+                </template>
+            </div>
         </div>
-        <PirmaryButtom
-          class="inline-flex items-center mt-10 py-5 px-8 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-        >
-          <Link  :href="canRegister ? route('register'):route('dashboard')" class="">
-            Get Started
-          </Link>
-        </PirmaryButtom>
-      </div>
+        <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+            <div class="text-center">
+                <h1 class="text-4xl font-extrabold text-gray-900 sm:text-5xl">
+                    Welcome to RCRM Storage
+                </h1>
+                <p class="mt-4 text-lg text-gray-600">
+                    Store, share, and collaborate on files and folders from your
+                    mobile device, tablet, or computer
+                </p>
+                <div class="mt-6">
+                    <p
+                        class="text-base font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                        We are delighted to have you here!
+                    </p>
+                </div>
+                <Link
+                    :href="canRegister ? route('register') : route('dashboard')"
+                    class=""
+                >
+                    <PirmaryButtom
+                        class="inline-flex items-center mt-10 py-5 px-8 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                    >
+                        Get Started
+                    </PirmaryButtom>
+                </Link>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style>
 .bg-dots-darker {
-  background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
+    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
 }
 @media (prefers-color-scheme: dark) {
-  .dark\:bg-dots-lighter {
-    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
-  }
+    .dark\:bg-dots-lighter {
+        background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
+    }
 }
 </style>
